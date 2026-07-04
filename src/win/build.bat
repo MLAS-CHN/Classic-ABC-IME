e7e745f4b8bc40d3a4ad3e94f28214d52ad3eef9
MLAS_CHN <2582105919@qq.com>
2026-07-05 01:50:44 +0800

    添加 build.bat 一键编译脚本

--- src/win/build.bat
+++ src/win/build.bat
@@ -1,24 +1,24 @@
 @echo off
-setlocal
-call "%~dp0env.bat"
+set "MSBUILD=D:\VisualStudio\MSBuild\Current\Bin\MSBuild.exe"
+set "SOLUTION=%~dp0abcime.sln"
 
-where msbuild >nul 2>&1
-if %errorlevel% neq 0 (
-  set "MSBUILD=D:\VisualStudio\MSBuild\Current\Bin\MSBuild.exe"
-) else (
-  set "MSBUILD=msbuild"
-)
+echo === Building ÖÇÄÜABC (x64) ===
+"%MSBUILD%" "%SOLUTION%" /p:Configuration=Release /p:Platform=x64 /m /t:Rebuild
+if %errorlevel% neq 0 goto :error
 
-echo Building Release x64...
-"%MSBUILD%" "%~dp0weasel.sln" /p:Configuration=Release /p:Platform=x64 /m /t:Rebuild /nologo /v:m
-if %errorlevel% neq 0 exit /b %errorlevel%
+echo.
+echo === Building ÖÇÄÜABC (Win32) ===
+"%MSBUILD%" "%SOLUTION%" /p:Configuration=Release /p:Platform=Win32 /m /t:Rebuild
+if %errorlevel% neq 0 goto :error
 
-echo Building Release Win32...
-"%MSBUILD%" "%~dp0weasel.sln" /p:Configuration=Release /p:Platform=Win32 /m /t:Rebuild /nologo /v:m
-if %errorlevel% neq 0 exit /b %errorlevel%
+echo.
+echo === Build complete ===
+echo Output: %~dp0..\..\output\
+pause
+exit /b 0
 
-del /q "%~dp0output\*.pdb" "%~dp0output\*.lib" "%~dp0output\*.exp" 2>nul
-rmdir /s /q "%~dp0msbuild" 2>nul
-
-echo Done.
-dir /b "%~dp0output\*.dll" "%~dp0output\*.ime" 2>nul
+:error
+echo.
+echo === Build FAILED ===
+pause
+exit /b 1
