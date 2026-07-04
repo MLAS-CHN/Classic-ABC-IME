af18b160901e0edd7ca8d0414e589177edd7d971
MLAS_CHN <2582105919@qq.com>
2026-07-05 01:54:33 +0800

    移动解决方案文件到根目录

--- build.bat
+++ build.bat
@@ -1,22 +1,24 @@
 @echo off
-cd /d "%~dp0src\win"
-if exist build rmdir /S /Q build
-mkdir build
-xcopy /E /I /Y ..\..\res build\res >nul
-xcopy /E /I /Y ..\..\data build\data >nul
+set "MSBUILD=D:\VisualStudio\MSBuild\Current\Bin\MSBuild.exe"
+set "SOLUTION=%~dp0abcime.sln"
 
-call "D:\VisualStudio\VC\Auxiliary\Build\vcvars64.bat"
-
-set CL=/I.. /I. /EHsc /O2 /std:c++17 /utf-8
-set ENGINE=..\candidate_item.cpp ..\pinyin_composition.cpp ..\pinyin_data.cpp ..\pinyin_file_io.cpp ..\pinyin_matcher.cpp ..\pinyin_split.cpp ..\pinyin_virtual_cursor.cpp ..\word_matcher.cpp ..\util.cpp
-set LNK=gdiplus.lib ole32.lib user32.lib gdi32.lib advapi32.lib oleaut32.lib
-
-cl /LD %ENGINE% proto.cpp proto_core.cpp proto_engine.cpp proto_ui.cpp /Fe:build\proto.dll /link /DEF:proto.def %LNK%
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-cl install_proto.cpp /Fe:build\install_proto.exe /link ole32.lib user32.lib /SUBSYSTEM:WINDOWS
-cl uninstall_proto.cpp /Fe:build\uninstall_proto.exe /link ole32.lib user32.lib /SUBSYSTEM:WINDOWS
+echo === Building ????ABC (x64) ===
+"%MSBUILD%" "%SOLUTION%" /p:Configuration=Release /p:Platform=x64 /m /t:Rebuild
+if %errorlevel% neq 0 goto :error
 
 echo.
-echo ==== Build complete ====
-dir build\*.dll build\*.exe
+echo === Building ????ABC (Win32) ===
+"%MSBUILD%" "%SOLUTION%" /p:Configuration=Release /p:Platform=Win32 /m /t:Rebuild
+if %errorlevel% neq 0 goto :error
+
+echo.
+echo === Build complete ===
+echo Output: %~dp0output\
+pause
+exit /b 0
+
+:error
+echo.
+echo === Build FAILED ===
+pause
+exit /b 1
